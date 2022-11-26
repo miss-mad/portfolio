78dd96cd1dcdbd31f4c68b1e5f1a44bd0e65660d
miss-mad <mmlh.harris@gmail.com>
2022-11-25 23:57:51 -0500

    completed navbar & started styling with bootstrap

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,59 @@
-import React from 'react';
+import React from "react";
 
-function NavBar({}) {
-    return (
-        
-    )
+export default function NavBar({ currentPage, handleNavBarChange }) {
+  return (
+    <ul className="nav nav-pills">
+      <div>This is NavBar</div>
+      <li>
+        <a
+          href="#aboutme"
+          onClick={() => {
+            handleNavBarChange("AboutMe");
+          }}
+          // insert different styling for if this tab is active
+          className={currentPage === "AboutMe" ? "nav-link active" : "nav-link"}
+        >
+          About Me
+        </a>
+      </li>
+      <li>
+        <a
+          href="#portfolio"
+          onClick={() => {
+            handleNavBarChange("Portfolio");
+          }}
+          // insert different styling for if this tab is active
+          className={
+            currentPage === "Portfolio" ? "nav-link active" : "nav-link"
+          }
+        >
+          Portfolio
+        </a>
+      </li>
+      <li>
+        <a
+          href="#contact"
+          onClick={() => {
+            handleNavBarChange("Contact");
+          }}
+          // insert different styling for if this tab is active
+          className={currentPage === "Contact" ? "nav-link active" : "nav-link"}
+        >
+          Contact
+        </a>
+      </li>
+      <li>
+        <a
+          href="#resume"
+          onClick={() => {
+            handleNavBarChange("Resume");
+          }}
+          // insert different styling for if this tab is active
+          className={currentPage === "Resume" ? "nav-link active" : "nav-link"}
+        >
+          Resume
+        </a>
+      </li>
+    </ul>
+  );
 }
