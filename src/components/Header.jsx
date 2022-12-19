dd868cb2a28c4ae16644875d06620db981ea344d
miss-mad <mmlh.harris@gmail.com>
2022-12-19 15:32:57 -0500

    use ant design for header (navbar) and footer

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,45 +1,42 @@
 import React from "react";
 import { Link, Outlet, useNavigate } from "react-router-dom";
+import { Menu } from "antd";
 
-export default function Header({ currentPage, handleNavBarChange }) {
-  // const navBarItems = [
-  //   {
-  //     label: "AboutMe",
-  //     key: "/",
-  //   },
-  //   {
-  //     label: "Portfolio",
-  //     key: "/portfolio",
-  //   },
-  //   {
-  //     label: "Contact",
-  //     key: "/contact",
-  //   },
-  //   {
-  //     label: "Resume",
-  //     key: "/resume",
-  //   },
-  // ];
+export default function Header() {
+  const navigate = useNavigate();
+  const handleNavBarChange = ({ key }) => {
+    navigate(key);
+  };
 
-  // const navigate = useNavigate();
-
-  // const handleNavBarChangeAgain = ({ key }) => {
-  //   navigate(key);
-  // };
+  const navBarItems = [
+    {
+      label: "AboutMe",
+      key: "/",
+    },
+    {
+      label: "Portfolio",
+      key: "/portfolio",
+    },
+    {
+      label: "Contact",
+      key: "/contact",
+    },
+    {
+      label: "Resume",
+      key: "/resume",
+    },
+  ];
 
   return (
     <div className="container">
       <h1>Madeleine's Portfolio</h1>
-      {/* <NavBar
-        currentPage={currentPage}
-        handleNavBarChange={handleNavBarChangeAgain}
-      /> */}
       <nav>
-        <Link to="/">About Me</Link>
-        <Link to="/portfolio">Portfolio</Link>
-        <Link to="/contact">Contact</Link>
-        <Link to="/resume">Resume</Link>
-        <Outlet />
+        <Menu
+          onClick={handleNavBarChange}
+          items={navBarItems}
+          mode="horizontal"
+          defaultSelectedKeys={"/"}
+        />
       </nav>
     </div>
   );
