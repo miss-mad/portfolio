b90d380f25b9a02a762cb364afdcd1df05e5f914
miss-mad <mmlh.harris@gmail.com>
2023-01-27 12:31:03 -0500

    updated skills in resume section

--- src/components/pages/Resume.jsx
+++ src/components/pages/Resume.jsx
@@ -2,163 +2,108 @@ import React, { useState } from "react"
 import resume from "../assets/Harris_Resume.pdf"
 import { Card, Col, List, Row } from "antd"
 const { Meta } = Card
-// import { listOfProficiencies as proficiencies } from "../listofProficiencies";
 
 export default function Resume() {
-  const listOfProficiencies = [
-    { id: 1, name: "Git" },
-    { id: 2, name: "HTML5" },
-    { id: 3, name: "CSS3" },
-    { id: 4, name: "JavaScript" },
-    { id: 5, name: "SQL" },
-    { id: 6, name: "NoSQL" },
-    { id: 7, name: "GraphQL" },
-    { id: 8, name: "CSS Frameworks" },
-    { id: 9, name: "MySQL" },
-    { id: 10, name: "Apollo" },
-    { id: 11, name: "jQuery" },
-    { id: 12, name: "Sequelize" },
-    { id: 13, name: "MongoDB and Mongoose" },
-    { id: 14, name: "React" },
-    { id: 15, name: "Mobile-First Design" },
-    { id: 16, name: "APIs - client, third party and server-side" },
-    { id: 17, name: "Agile Methodology" },
-    { id: 18, name: "Github and Heroku Deployment" },
-  ]
-
-  const listTitleData = [
-    { title: "1", content: "contentcard1" },
-    { title: "2" },
-    { title: "3" },
-    { title: "4" },
-  ]
-
-  const listContentData = [
-    { description: "descr 1" },
-    { description: "descr 2" },
-    { description: "descr 3" },
-    { description: "descr 4" },
-  ]
-
-  const tabList = [
+  const cardData = [
     {
-      key: "tab1",
-      tab: "tab1",
+      key: "a",
+      cardTitle: "Languages",
+      listData: [
+        { key: 1, name: "Git" },
+        { key: 2, name: "HTML5" },
+        { key: 3, name: "CSS3" },
+        { key: 4, name: "JavaScript" },
+        { key: 5, name: "SQL" },
+        { key: 6, name: "NoSQL" },
+        { key: 7, name: "GraphQL" },
+      ],
     },
     {
-      key: "tab2",
-      tab: "tab2",
+      key: "b",
+      cardTitle: "Frameworks",
+      listData: [
+        { key: 8, name: "Materialize" },
+        { key: 9, name: "Bootstrap" },
+        { key: 10, name: "Express" },
+        { key: 11, name: "MySQL" },
+      ],
+    },
+    {
+      key: "c",
+      cardTitle: "Libraries",
+      listData: [
+        { key: 12, name: "jQuery" },
+        { key: 13, name: "Sequelize" },
+        { key: 14, name: "MongoDB and Mongoose" },
+        { key: 15, name: "Apollo" },
+        { key: 16, name: "React" },
+        { key: 17, name: "Ant Design" },
+      ],
+    },
+    {
+      key: "d",
+      cardTitle: "Core Concepts",
+      listData: [
+        { key: 18, name: "Mobile-First Design" },
+        { key: 19, name: "Local and Session Storage" },
+        { key: 20, name: "APIs" },
+        { key: 21, name: "User Authentication" },
+        { key: 22, name: "Agile Methodology" },
+        { key: 23, name: "PWAs" },
+        { key: 24, name: "Github and Heroku Deployment" },
+      ],
     },
   ]
-
-  const contentList = {
-    tab1: <p>content1</p>,
-    tab2: <p>content2</p>,
-  }
-
-  const [activeTabKey, setActiveTabKey] = useState("tab1")
-  const onTab1Change = (key) => {
-    setActiveTabKey(key)
-  }
-
-  // {listOfProficiencies.map((proficiency) => {
-  //   return (
-  //     <ul className="proficiency-list">
-  //       <li>{proficiency.name}</li>
-  //     </ul>
-  //   )
-  // })}
-
-  const testObj = {
-    title: "titletest",
-    content: "contenttest",
-  }
-
-  const testArr = ["contenttestarr1", "contenttestarr2"]
 
   return (
-    <section className="container">
-      <h3>Resume</h3>
-      <a
-        href={resume}
-        className="downloadResume"
-        target="_blank"
-        rel="noopener noreferrer"
-        download
-      >
-        Downloadable resume here
-      </a>
+    <section className="container-no-flex">
+      <div className="resume-headers">
+        <h3>Resume</h3>
+        <a
+          href={resume}
+          className="downloadResume"
+          target="_blank"
+          rel="noopener noreferrer"
+          download
+          style={{ fontSize: "32px" }}
+        >
+          Downloadable resume here
+        </a>
 
-      <h5>List of Proficiencies</h5>
-
-      <div className="site-card-wrapper">
-        <Row gutter={16}>
-          <Col span={8}>
-            <Card title="Card title" bordered={false}>
-              Card content
-              <div>
-                {testObj.title}
-                {testObj.content}
-                <div>{testArr}</div>
-              </div>
-            </Card>
-          </Col>
-          <Col span={8}>
-            <Card title="Card title" bordered={false}>
-              Card content
-              {testObj.title}
-              {testObj.content}
-            </Card>
-          </Col>
-          <Col span={8}>
-            <Card title="Card title" bordered={false}>
-              Card content
-              {testObj.title}
-              {testObj.content}
-            </Card>
-          </Col>
-        </Row>
+        <h5>List of Proficiencies</h5>
       </div>
 
-      <List
-        grid={{ gutter: 16, xs: 1, sm: 2, md: 4, lg: 4, xl: 6, xxl: 3 }}
-        dataSource={listTitleData}
-        renderItem={(item) => (
-          <List.Item>
-            <Card
-              title={item.title}
-              tabList={tabList}
-              activeTabKey={activeTabKey}
-              onTabChange={(key) => {
-                onTab1Change(key)
+      {cardData.map((cardData) => {
+        return (
+          <Card
+            key={cardData.key}
+            title={cardData.cardTitle}
+            className="my-2"
+            size="small"
+          >
+            <List
+              grid={{
+                gutter: 16,
+                xs: 1,
+                sm: 2,
+                md: 4,
+                lg: 4,
+                xl: 6,
+                xxl: 3,
+                column: 3,
               }}
-            >
-              <Meta description={item.description} />
-              {listContentData[activeTabKey]}
-            </Card>
-          </List.Item>
-        )}
-      />
-
-      <Card
-        title="test"
-        tabList={tabList}
-        activeTabKey={activeTabKey}
-        onTabChange={(key) => {
-          onTab1Change(key)
-        }}
-      >
-        {contentList[activeTabKey]}
-      </Card>
+              dataSource={cardData.listData}
+              renderItem={(listData) => {
+                return (
+                  <List.Item>
+                    <Meta key={listData.key} description={listData.name} />
+                  </List.Item>
+                )
+              }}
+            />
+          </Card>
+        )
+      })}
     </section>
   )
-}
-
-{
-  /* <List
-        size="small"
-        bordered
-        dataSource={listContentData}
-        renderItem={(item) => <List.Item>{item}</List.Item>}
-      /> */
 }
