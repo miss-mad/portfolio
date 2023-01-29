a6bfb2cfab5ad8eb68b46f3b2fef022b42411720
miss-mad <mmlh.harris@gmail.com>
2023-01-28 22:25:58 -0500

    working on project section cards

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,36 +1,83 @@
-import React from "react";
+import React from "react"
+import { Avatar, Card, Popover } from "antd"
+import { LinkOutlined, GithubOutlined } from "@ant-design/icons"
+
+const { Meta } = Card
 
 export default function Project({
+  key,
   name,
   index,
-  key,
   className,
   linkGithubRepo,
   linkDeployedApp,
   backgroundImage,
-  handleClick,
 }) {
+  const handleDeployedAppClick = (e) => {
+    console.log(e)
+  }
+
+  const handleGithubRepoClick = (e) => {
+    console.log(e)
+  }
   return (
-    <section
-      className={`container project-card ${className}`}
-      style={{ backgroundImage: backgroundImage }}
-      onClick={handleClick}
-    >
-      <div className="row">
-        <h3>{name}</h3>
+    <div>
+      <section
+        className={`container project-card ${className}`}
+        style={{ backgroundImage: backgroundImage }}
+      >
+        <div className="row">
+          <h3>{name}</h3>
 
-        <button className="btn">
-          <a href={linkDeployedApp} target="_blank" rel="noopener noreferrer">
-            Deployed Application Link
-          </a>
-        </button>
+          <button className="btn">
+            <a href={linkDeployedApp} target="_blank" rel="noopener noreferrer">
+              Deployed Application Link
+            </a>
+          </button>
 
-        <button className="btn">
-          <a href={linkGithubRepo} target="_blank" rel="noopener noreferrer">
-            Github Repo Link
-          </a>
-        </button>
-      </div>
-    </section>
-  );
+          <button className="btn">
+            <a href={linkGithubRepo} target="_blank" rel="noopener noreferrer">
+              Github Repo Link
+            </a>
+          </button>
+        </div>
+      </section>
+
+      <Card
+        style={{
+          width: 240,
+          backgroundImage: backgroundImage,
+        }}
+        hoverable
+      >
+        <Meta title={name} description={(linkDeployedApp, linkGithubRepo)} />
+      </Card>
+
+      <Card
+        style={{
+          width: 300,
+          backgroundImage: backgroundImage,
+        }}
+        actions={[
+          <Popover
+            content="Deployed Application"
+            trigger="hover"
+            placement="bottom"
+          >
+            <LinkOutlined key="deployed" onClick={handleDeployedAppClick} />
+          </Popover>,
+          <Popover content="Github Repo" trigger="hover" placement="bottom">
+            <GithubOutlined key="github" onClick={handleGithubRepoClick} />
+          </Popover>,
+        ]}
+        hoverable
+      >
+        <Meta
+          avatar={<Avatar src="https://joeschmoe.io/api/v1/random" />}
+          title={name}
+          description={(linkDeployedApp, linkGithubRepo)}
+        />
+      </Card>
+    </div>
+  )
 }
--- src/components/projectData.js
+++ src/components/projectData.js
@@ -1,30 +1,47 @@
 // can refactor to store in a database once we learn it later on
 
+const images = [
+  { key: 0, source: "./assets/project_screenshots/resumade_screenshot1.png" },
+  { key: 1, source: "./assets/project_screenshots/whos_app_screenshot1.png" },
+  {
+    key: 2,
+    source: "./assets/project_screenshots/mthsy_music_final_screenshot1.JPG",
+  },
+  {
+    key: 3,
+    source: "./assets/project_screenshots/mthsy_music_final_screenshot1.JPG",
+  },
+  {
+    key: 4,
+    source: "./assets/project_screenshots/mthsy_music_final_screenshot1.JPG",
+  },
+  {
+    key: 5,
+    source: "./assets/project_screenshots/mthsy_music_final_screenshot1.JPG",
+  },
+]
+
 const projectData = [
   {
     id: 0,
     name: "ResuMade",
     linkDeployedApp: "https://resumade.herokuapp.com/",
     linkGithubRepo: "https://github.com/PeytonCast/ResuMade",
-    backgroundImage:
-      "url('https://images.unsplash.com/photo-1446038236174-69712e24d137?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fG9jZWFufGVufDB8MXwwfHw%3D&auto=format&fit=crop&w=800&q=60')",
+    backgroundImage: "url(" + require(`${images[0].source}`) + ")",
   },
   {
     id: 1,
     name: "Who's App",
     linkDeployedApp: "https://whooooos-app.herokuapp.com",
     linkGithubRepo: "https://github.com/miss-mad/Whos-App",
-    backgroundImage:
-      "url('https://images.unsplash.com/photo-1528543606781-2f6e6857f318?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YWR2ZW50dXJlfGVufDB8MXwwfHw%3D&auto=format&fit=crop&w=800&q=60')",
+    backgroundImage: "url(" + require(`${images[1].source}`) + ")",
   },
   {
     id: 2,
     name: "MTHSY Music!",
     linkDeployedApp: "https://miss-mad.github.io/mthsy-music",
     linkGithubRepo: "https://github.com/miss-mad/mthsy-music",
-    backgroundImage:
-      "url('https://images.unsplash.com/photo-1595960684234-49d2a004e753?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjl8fGFkdmVudHVyZXxlbnwwfDF8MHx8&auto=format&fit=crop&w=800&q=60')",
-    // backgroundImage: "url('')",
+    backgroundImage: "url(" + require(`${images[2].source}`) + ")",
   },
   {
     id: 3,
@@ -32,16 +49,14 @@ const projectData = [
     linkDeployedApp:
       "https://drive.google.com/file/d/1i9vDdDTLYLmDfU2recQM0PSODGzLyGBb/view",
     linkGithubRepo: "https://github.com/miss-mad/birds-eye-for-business",
-    backgroundImage:
-      "url('https://images.unsplash.com/photo-1535498730771-e735b998cd64?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c2t5bGluZXxlbnwwfDF8MHx8&auto=format&fit=crop&w=800&q=60')",
+    backgroundImage: "url(" + require(`${images[3].source}`) + ")",
   },
   {
     id: 4,
     name: "Annotate and Alter",
     linkDeployedApp: "https://annotate-and-alter-text-editor.herokuapp.com",
     linkGithubRepo: "https://github.com/miss-mad/annotate-and-alter",
-    backgroundImage:
-      "url('https://images.unsplash.com/photo-1499363536502-87642509e31b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDd8fGFkdmVudHVyZXxlbnwwfDF8MHx8&auto=format&fit=crop&w=800&q=60')",
+    backgroundImage: "url(" + require(`${images[4].source}`) + ")",
   },
   {
     id: 5,
@@ -49,15 +64,18 @@ const projectData = [
     linkDeployedApp:
       "https://drive.google.com/file/d/1UB6qB8Fz7ZMcLKRw_VjJcDwgSqVeXZAs/view",
     linkGithubRepo: "https://github.com/miss-mad/shoot-the-breeze",
-    backgroundImage:
-      "url('https://images.unsplash.com/photo-1446038236174-69712e24d137?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fG9jZWFufGVufDB8MXwwfHw%3D&auto=format&fit=crop&w=800&q=60')",
+    backgroundImage: "url(" + require(`${images[5].source}`) + ")",
   },
   // {
-  //   id: 7,
+  //   id: 6,
   //   name: "Confer the Code",
   //   linkDeployedApp: "https://confer-the-code.herokuapp.com",
   //   linkGithubRepo: "https://github.com/miss-mad/confer-the-code",
+  //   backgroundImage: "url(" + require(`${images[6].source}`) + ")",
   // },
 ]
 
 export default projectData
+
+// readme dynamically (node), roster ready (OOP), bird's eye for business (mysql), regex tutorial
+// shopaholic (ORM), confer the code (?) (MVC), shoot the breeze (nosql)
